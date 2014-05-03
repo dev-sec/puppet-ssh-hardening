5c6ef678ddb321f92e883746c5fa64f22da97a60
Dominik Richter <dominik.richter@gmail.com>
2014-05-04 00:20:57 +0200

    created basic structure to reimport current hardening project
    the current ssh hardening project doesn't hadhere to the styling and structure of modules which we require for this project. it must be created from the ground up to ensure all pieces are in place. the old configuration will now be imported on top
    
    also note: unlike the default structure this one doesn't contain spec and tests. we want to unify everything under a common test framework for integration, so the relevance of these must be checked.
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

---
+++ Modulefile
@@ -0,0 +1,8 @@
+name    'ssh_hardening'
+version '0.1.0'
+source 'https://github.com/TelekomLabs/puppet-ssh-hardening'
+author 'Dominik Richter'
+license 'Apache License, Version 2.0'
+summary 'Installs and configures OpenSSH with hardening'
+description 'Installs and configures OpenSSH with hardening'
+project_page 'https://github.com/TelekomLabs/puppet-ssh-hardening'
---
+++ manifests/init.pp
@@ -0,0 +1,29 @@
+# == Class: ssh
+#
+# The default SSH class which installs both SSH client and server configurations.
+#
+# === Parameters
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { ssh }
+#
+# === Copyright
+#
+# Copyright 2014, Deutsche Telekom AG
+#
+class ssh_hardening {
+}
