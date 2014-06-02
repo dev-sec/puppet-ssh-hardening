662782c71ce678d2d305dcc8bef319bd6876b401
Dominik Richter <dominik.richter@gmail.com>
2014-06-02 16:43:31 +0200

    feature: add cipher configuration for redhat and centos
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/puppet/parser/functions/get_ssh_ciphers.rb
+++ lib/puppet/parser/functions/get_ssh_ciphers.rb
@@ -3,9 +3,9 @@ Puppet::Parser::Functions::newfunction(:get_ssh_ciphers, :type => :rvalue) do |a
   osrelease = args[1]
   weak_ciphers = args[2] ? 'weak' : 'default'
 
-  ciphers_59 = {}
-  ciphers_59.default = 'aes256-ctr,aes192-ctr,aes128-ctr'
-  ciphers_59['weak'] = ciphers_59['default'] + ',aes256-cbc,aes192-cbc,aes128-cbc'
+  ciphers_53 = {}
+  ciphers_53.default = 'aes256-ctr,aes192-ctr,aes128-ctr'
+  ciphers_53['weak'] = ciphers_53['default'] + ',aes256-cbc,aes192-cbc,aes128-cbc'
 
   ciphers_66 = {}
   ciphers_66.default = 'chacha20-poly1305@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr'
@@ -13,16 +13,23 @@ Puppet::Parser::Functions::newfunction(:get_ssh_ciphers, :type => :rvalue) do |a
 
   # creat the default version map (if os + version are default)
   default_vmap = {}
-  default_vmap.default = ciphers_59
+  default_vmap.default = ciphers_53
   
   # create the main map
   m = {}
   m.default = default_vmap
 
   m['ubuntu'] = {}
-  m['ubuntu']['12.04'] = ciphers_59
+  m['ubuntu']['12.04'] = ciphers_53
   m['ubuntu']['14.04'] = ciphers_66
-  m['ubuntu'].default = ciphers_59
+  m['ubuntu'].default = ciphers_53
+
+  m['redhat'] = {}
+  m['redhat']['6.4'] = ciphers_53
+  m['redhat']['6.5'] = ciphers_53
+  m['redhat'].default = ciphers_53
+
+  m['centos'] = m['redhat']
 
   m[os][osrelease][weak_ciphers]
 end
