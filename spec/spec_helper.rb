f11bfe883a4cdfdf543118ffa1f3650d43476983
Dominik Richter <dominik.richter@gmail.com>
2015-05-08 14:05:53 +0200

    add environment path for specs
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,7 @@ fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
+  c.environmentpath = File.expand_path(File.join(Dir.pwd, 'spec'))
 end
 
 # Helper function to expect a class to have a set of
