ca577e410a5103df36e554090e2860bf1e2fbe87
Dominik Richter <dominik@vulcanosec.com>
2015-05-09 16:10:41 +0200

    improvement: add type information to specs
    i.e. stop writing everything as a string, but use numbers and values as they are set in Puppet classes
    
    Signed-off-by: Dominik Richter <dominik@vulcanosec.com>

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -35,11 +35,11 @@ describe 'ssh_hardening::server' do
   it { should contain_class('ssh::server').with_storeconfigs_enabled(false) }
 
   # default configuration
-  expect_option('ssh::server', 'Port', ['22'])
+  expect_option('ssh::server', 'Port', [22])
   # user configuration
   context 'with ports => [8022]' do
     let(:params) { { :ports => [8022] } }
-    expect_option('ssh::server', 'Port', ['8022'])
+    expect_option('ssh::server', 'Port', [8022])
   end
 
   # user configuration
@@ -57,19 +57,19 @@ describe 'ssh_hardening::server' do
   end
 
   # default configuration
-  expect_option('ssh::server', 'ClientAliveInterval', '600')
+  expect_option('ssh::server', 'ClientAliveInterval', 600)
   # user configuration
   context 'with client_alive_interval => 300' do
     let(:params) { { :client_alive_interval => 300 } }
-    expect_option('ssh::server', 'ClientAliveInterval', '300')
+    expect_option('ssh::server', 'ClientAliveInterval', 300)
   end
 
   # default configuration
-  expect_option('ssh::server', 'ClientAliveCountMax', '3')
+  expect_option('ssh::server', 'ClientAliveCountMax', 3)
   # user configuration
   context 'with client_alive_count => 2' do
     let(:params) { { :client_alive_count => 2 } }
-    expect_option('ssh::server', 'ClientAliveCountMax', '2')
+    expect_option('ssh::server', 'ClientAliveCountMax', 2)
   end
 
   # default configuration
